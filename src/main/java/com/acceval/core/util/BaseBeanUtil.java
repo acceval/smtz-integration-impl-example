651441f0b20f39bb60fe2265a1b1546f25d14740
Mun <thian.long.mun@acceval-intl.com>
2018-11-19 13:43:59 +0800

    move BaseBeanUtil to ms-util

---
+++ src/main/java/com/acceval/core/util/BaseBeanUtil.java
@@ -0,0 +1,58 @@
+package com.acceval.core.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.acceval.core.MicroServiceUtilException;
+import com.acceval.core.repository.BaseRepository;
+import com.acceval.core.util.ClassUtil;
+
+@Service
+public class BaseBeanUtil implements ApplicationContextAware {
+	private static Logger Logger = LoggerFactory.getLogger(BaseBeanUtil.class);
+
+	private static ApplicationContext context;
+	private static Map<String, Object> mapSpeialBean;
+	private static Repositories repositories;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+		mapSpeialBean = new HashMap<>();
+		repositories = new Repositories(applicationContext);
+	}
+
+	public static <T> T getBean(Class<T> beanClass) {
+		if (RestTemplate.class.getName().equals(beanClass.getName())) {
+			if (mapSpeialBean.get(RestTemplate.class.getName()) != null) {
+				return (T) mapSpeialBean.get(RestTemplate.class.getName());
+			} else {
+				RestTemplateBuilder builder = getBean(RestTemplateBuilder.class);
+				RestTemplate restTemplate = builder.build();
+				mapSpeialBean.put(RestTemplate.class.getName(), restTemplate);
+
+				return (T) restTemplate;
+			}
+		}
+		return context.getBean(beanClass);
+	}
+
+	public static BaseRepository<?> getRepositoryBaseOnEntityName(String entityName) {
+		try {
+			return (BaseRepository) repositories.getRepositoryFor(ClassUtil.getClass(entityName));
+		} catch (MicroServiceUtilException e) {
+			Logger.error(e.getMessage(), e);
+		}
+		return null;
+	}
+}
