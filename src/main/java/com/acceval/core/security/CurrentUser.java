c066d80a92663bb1b41f80e30ebb16533e5abca5
HONG ONN LIM <hongonn@192.168.0.128>
2018-12-27 02:24:30 +0800

    Move session info to ms-util.

---
+++ src/main/java/com/acceval/core/security/CurrentUser.java
@@ -0,0 +1,69 @@
+package com.acceval.core.security;
+
+import com.acceval.core.model.AuthUser;
+
+public class CurrentUser implements AuthUser {
+
+	private Long id;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Long companyId;
+	private String companyCode;
+	private boolean enabled;
+	
+	@Override
+	public String toString() {
+        return username;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(Long companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyCode() {
+		return companyCode;
+	}
+	public void setCompanyCode(String companyCode) {
+		this.companyCode = companyCode;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+}
---
+++ src/main/java/com/acceval/core/security/SessionUtil.java
@@ -0,0 +1,48 @@
+package com.acceval.core.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SessionUtil {
+
+
+	public static String getSchemaName() {
+				
+		String schemaName = "shared";
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (auth != null) {
+			Object principal = auth.getPrincipal();
+			
+			if (principal != null && principal instanceof CurrentUser) {
+				
+				CurrentUser currentUser = (CurrentUser) principal;
+				
+				if (currentUser.getCompanyCode() != null) {
+					
+					schemaName = currentUser.getCompanyCode().toLowerCase();			
+				}
+			}		
+		}
+		return schemaName;
+	}
+	
+
+	public static Long getCompanyId() {
+						
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (auth != null) {
+			Object principal = auth.getPrincipal();
+			
+			if (principal != null && principal instanceof CurrentUser) {
+				
+				CurrentUser currentUser = (CurrentUser) principal;
+				return currentUser.getCompanyId();
+			}
+		}
+		return null;
+	}	
+		
+}
