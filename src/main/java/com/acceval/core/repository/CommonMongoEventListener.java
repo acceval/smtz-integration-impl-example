18f5d702c300250ff4e7982ed737aa6f034f6e36
Mun <thian.long.mun@acceval-intl.com>
2019-05-03 12:25:12 +0800

    fix unable to save company id

--- src/main/java/com/acceval/core/repository/CommonMongoEventListener.java
+++ src/main/java/com/acceval/core/repository/CommonMongoEventListener.java
@@ -1,6 +1,7 @@
 package com.acceval.core.repository;
 
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,24 @@ import com.acceval.core.security.PrincipalUtil;
 
 @Component
 public class CommonMongoEventListener extends AbstractMongoEventListener<Object> {
+
+	@Override
+	public void onBeforeConvert(BeforeConvertEvent<Object> event) {
+		Object source = event.getSource();
+		if (source instanceof OwnerModel) {
+			OwnerModel ownerModel = (OwnerModel) source;
+			if (ownerModel.getRecordOwner() == null) {
+				ownerModel.setRecordOwner(PrincipalUtil.getUsername());
+			}
+		}
+
+		if (source instanceof BaseModel) {
+			BaseModel baseModel = (BaseModel) source;
+			if (baseModel.getCompanyId() == null) {
+				baseModel.setCompanyId(PrincipalUtil.getCompanyID());
+			}
+		}
+	}
 
 	@Override
 	public void onBeforeSave(BeforeSaveEvent<Object> event) {
