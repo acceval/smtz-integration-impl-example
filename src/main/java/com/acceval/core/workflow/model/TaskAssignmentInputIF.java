2d705d77e5990d1252923f78be8e122bccdc2533
Mun <thian.long.mun@acceval-intl.com>
2019-06-18 16:07:45 +0800

    TaskAssignmentInputIF

---
+++ src/main/java/com/acceval/core/workflow/model/TaskAssignmentInputIF.java
@@ -0,0 +1,26 @@
+package com.acceval.core.workflow.model;
+
+public interface TaskAssignmentInputIF {
+
+	final static String ASSIGN_USER_ID = "assignUserID";
+
+	enum TaskAssignment {
+		REPORT_TO("REPORT_TO", "Report To"),
+		DIRECT_TO("DIRECT_TO", "Direct To"),
+		DESIGNATION("DESIGNATION", "Designation"),
+		CONDITION_TABLE("CONDITION_TABLE", "Condition Table");
+
+		public final String code;
+		public final String name;
+		private TaskAssignment(String code, String name) {
+			this.code = code;
+			this.name = name;
+		}
+	}
+	
+	TaskAssignment getTaskAssignment();
+	
+	String getTaskAssignParam1();
+	
+	String getTaskAssignParam2();
+}
