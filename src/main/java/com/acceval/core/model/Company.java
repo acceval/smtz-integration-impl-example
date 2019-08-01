992aa4084ac2a190112f56062db3939220518c0a
Hazzrat <hazzrat.eemam@acceval-intl.com>
2019-08-01 16:14:31 +0800

    Get company for RemoteServerTemplate.

---
+++ src/main/java/com/acceval/core/model/Company.java
@@ -0,0 +1,29 @@
+package com.acceval.core.model;
+
+public class Company {
+	
+	private Long id;
+	private String code;
+	private String servicePackage;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getServicePackage() {
+		return servicePackage;
+	}
+	public void setServicePackage(String servicePackage) {
+		this.servicePackage = servicePackage;
+	}
+	
+	
+
+}
