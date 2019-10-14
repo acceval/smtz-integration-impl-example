82be00c99b04a5262d0c7ecaa2ae9e9878525e71
YK <yoong.khang.ching@acceval-intl.com>
2019-10-14 14:45:28 +0800

    added schema name.

--- src/main/java/com/acceval/core/security/RequestInterceptorUtil.java
+++ src/main/java/com/acceval/core/security/RequestInterceptorUtil.java
@@ -1,16 +1,17 @@
 package com.acceval.core.security;
 
-import feign.RequestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import feign.RequestTemplate;
 
 public class RequestInterceptorUtil {
 	private static final Logger logger = LoggerFactory.getLogger(RequestInterceptorUtil.class);
 
 	public static void apply(RequestTemplate template) {
-		
+
 		String token = PrincipalUtil.getToken();
-		
+
 		if (token != null) {
 
 //			template.header("Authorization", token);
@@ -19,6 +20,7 @@ public class RequestInterceptorUtil {
 
 			template.header(PrincipalUtil.HDRKEY_COMPANYID, String.valueOf(user.getCompanyId()));
 			template.header(PrincipalUtil.HDRKEY_COMPANYCODE, user.getCompanyCode());
+			template.header(PrincipalUtil.HDRKEY_SCHEMANAME, user.getSchemaName());
 			if (user.getServicePackage() != null) {
 				template.header(PrincipalUtil.HDRKEY_SERVICEPACKAGE, user.getServicePackage().toString());
 			}
@@ -29,12 +31,14 @@ public class RequestInterceptorUtil {
 			if (user != null) {
 				template.header(PrincipalUtil.HDRKEY_COMPANYID, String.valueOf(user.getCompanyId()));
 				template.header(PrincipalUtil.HDRKEY_COMPANYCODE, user.getCompanyCode());
+				template.header(PrincipalUtil.HDRKEY_SCHEMANAME, user.getSchemaName());
 				if (user.getServicePackage() != null) {
 					template.header(PrincipalUtil.HDRKEY_SERVICEPACKAGE, user.getServicePackage().toString());
 				}
 			} else {
 				template.header(PrincipalUtil.HDRKEY_COMPANYID, "");
 				template.header(PrincipalUtil.HDRKEY_COMPANYCODE, "");
+				template.header(PrincipalUtil.HDRKEY_SCHEMANAME, "");
 			}
 		}
 	}
