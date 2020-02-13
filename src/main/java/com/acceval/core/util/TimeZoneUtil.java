79d495dbb87f845e6f682fa17934499bce7f60b0
ks <ksing97.ks@gmail.com>
2020-02-13 16:55:21 +0800

    add new method.

--- src/main/java/com/acceval/core/util/TimeZoneUtil.java
+++ src/main/java/com/acceval/core/util/TimeZoneUtil.java
@@ -5,10 +5,10 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-import com.acceval.core.service.TimezoneServiceImpl;
 import org.apache.commons.lang3.StringUtils;
 
 import com.acceval.core.security.PrincipalUtil;
+import com.acceval.core.service.TimezoneServiceImpl;
 
 public class TimeZoneUtil {
 	public static String convertTimeZone(String localDateString) {
@@ -31,5 +31,31 @@ public class TimeZoneUtil {
 			}
 		}
 		return localDateString;
+	}
+
+	public static LocalDateTime returnTimeZone(String localDateString) {
+		String timeZone = PrincipalUtil.getTimeZone();
+
+		if (StringUtils.isBlank(timeZone)) {
+			timeZone = PrincipalUtil.getSystemUser() != null ? PrincipalUtil.getSystemUser().getTimeZone() : "";
+		}
+
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		if (StringUtils.isNotBlank(timeZone)) {
+			if (localDateString.length() == 10) {
+				return LocalDate.parse(localDateString, dateFormatter).atStartOfDay().atZone(ZoneId.of(timeZone))
+						.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+			} else {
+				return LocalDateTime.parse(localDateString, dateTimeFormatter).atZone(ZoneId.of(timeZone))
+						.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+			}
+		} else {
+			if (localDateString.length() == 10) {
+				return LocalDate.parse(localDateString, dateFormatter).atStartOfDay();
+			} else {
+				return LocalDateTime.parse(localDateString, dateTimeFormatter);
+			}
+		}
 	}
 }
