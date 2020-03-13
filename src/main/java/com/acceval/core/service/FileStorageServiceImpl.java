2f07a62e6b9a500b1fdf24b88de772be0540ef50
Hong Onn Lim <hongonn@yahoo.com>
2020-03-13 13:23:57 +0800

    Pdf ssl workaround.

--- src/main/java/com/acceval/core/service/FileStorageServiceImpl.java
+++ src/main/java/com/acceval/core/service/FileStorageServiceImpl.java
@@ -165,7 +165,7 @@ public class FileStorageServiceImpl implements FileStorageService {
     @Override
     public Resource loadAsResource(String filename) {
         try {
-            Path file = load(filename);
+            Path file = this.load(filename);
             Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
