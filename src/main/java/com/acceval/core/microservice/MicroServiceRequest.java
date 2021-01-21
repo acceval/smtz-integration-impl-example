754685241a5d7cfa4a715e921e87f49023fb4ce3
Mun <thian.long.mun@acceval-intl.com>
2021-01-21 16:30:25 +0800

    fix for MicroServiceUtil

--- src/main/java/com/acceval/core/microservice/MicroServiceRequest.java
+++ src/main/java/com/acceval/core/microservice/MicroServiceRequest.java
@@ -1,27 +1,18 @@
 package com.acceval.core.microservice;
 
-import java.util.Optional;
-
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.client.RestTemplate;
 
 import com.acceval.core.MicroServiceUtilException;
 
 public class MicroServiceRequest {
 
-	private DiscoveryClient discoveryClient;
-	private RestTemplate restTemplate;
 	private String token;
 	private String msService;
 	private String msFunction;
 	private String param;
 
-	public MicroServiceRequest(DiscoveryClient discoveryClient, RestTemplate restTemplate, String token, String msService,
-			String msFunction, String param) {
+	public MicroServiceRequest(String token, String msService, String msFunction, String param) {
 		super();
-		this.discoveryClient = discoveryClient;
-		this.restTemplate = restTemplate;
 		this.token = token;
 		this.msService = msService;
 		this.msFunction = msFunction;
@@ -41,23 +32,9 @@ public class MicroServiceRequest {
 	}
 
 	public void assertNull(Class<?> clazz) throws MicroServiceUtilException {
-		Optional.ofNullable(discoveryClient).orElseThrow(() -> new MicroServiceUtilException(clazz, "DiscoveryClient is null."));
-		Optional.ofNullable(restTemplate).orElseThrow(() -> new MicroServiceUtilException(clazz, "RestTemplate is null."));
 		if (StringUtils.isBlank(msService)) new MicroServiceUtilException(clazz, "Microservice Service Name is null.");
 		if (StringUtils.isBlank(msFunction)) new MicroServiceUtilException(clazz, "Microservice Function is null.");
 		//		if (StringUtils.isBlank(param)) new MicroServiceUtilException(clazz,"Microservice Request is null.");
-	}
-
-	public DiscoveryClient getDiscoveryClient() {
-		return discoveryClient;
-	}
-
-	public void setDiscoveryClient(DiscoveryClient discoveryClient) {
-		this.discoveryClient = discoveryClient;
-	}
-
-	public RestTemplate getRestTemplate() {
-		return restTemplate;
 	}
 
 	public String getToken() {
@@ -66,10 +43,6 @@ public class MicroServiceRequest {
 
 	public void setToken(String token) {
 		this.token = token;
-	}
-
-	public void setRestTemplate(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
 	}
 
 	public String getMsService() {
--- src/main/java/com/acceval/core/microservice/MicroServiceUtil.java
+++ src/main/java/com/acceval/core/microservice/MicroServiceUtil.java
@@ -1,36 +1,37 @@
 package com.acceval.core.microservice;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.acceval.core.MicroServiceUtilException;
+import com.acceval.core.util.BaseBeanUtil;
 
+@Service
 public class MicroServiceUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MicroServiceUtil.class);
 
-	//@Value("${micoservice.http}")
-	//static private String http;
+	@Value("${micoservice.url}")
+	private String url;
 
-	public static Object getForObject(MicroServiceRequest microServiceRequest, Class<?> type)
+	public Object getForObject(MicroServiceRequest microServiceRequest, Class<?> type)
 			throws MicroServiceUtilException {
 		return getForObject(microServiceRequest, null, type);
 	}
 
-	public static Object getForObject(MicroServiceRequest microServiceRequest, MultiValueMap<String, String> mvmValue,
+	public Object getForObject(MicroServiceRequest microServiceRequest, MultiValueMap<String, String> mvmValue,
 			Class<?> type) throws MicroServiceUtilException {
 
 		/** null checking */
@@ -38,22 +39,22 @@ public class MicroServiceUtil {
 		Optional.ofNullable(type)
 				.orElseThrow(() -> new MicroServiceUtilException(MicroServiceUtil.class, "REST Type is null."));
 
-		DiscoveryClient discoveryClient = microServiceRequest.getDiscoveryClient();
-		RestTemplate restTemplate = microServiceRequest.getRestTemplate();
-		String zuulService = "ZUUL-SERVER";
+		//		DiscoveryClient discoveryClient = microServiceRequest.getDiscoveryClient();
+		RestTemplate restTemplate = BaseBeanUtil.getBean(OAuth2RestTemplate.class);
+		//		String zuulService = "ZUUL-SERVER";
 		String msService = microServiceRequest.getMsService();
 		String msFunction = microServiceRequest.getMsFunction();
 		String param = microServiceRequest.getParam() == null ? "" : microServiceRequest.getParam();
 		String token = microServiceRequest.getToken();
 
-		List<ServiceInstance> instances = discoveryClient.getInstances(zuulService);
-		if (instances.isEmpty())
-			throw new MicroServiceUtilException(MicroServiceUtil.class, zuulService + " Service is Not Available!");
-		ServiceInstance instance = instances.get(0);
-		String host = instance.getHost();
-		String url = "https://" + msService.replaceFirst(Pattern.quote("/"), "") + "/" + msFunction + "/" + param;
+		//		List<ServiceInstance> instances = discoveryClient.getInstances(zuulService);
+		//		if (instances.isEmpty())
+		//			throw new MicroServiceUtilException(MicroServiceUtil.class, zuulService + " Service is Not Available!");
+		//		ServiceInstance instance = instances.get(0);
+		//		String host = instance.getHost();
+		//		String url = "http://" + msService.replaceFirst(Pattern.quote("/"), "") + "/" + msFunction + "/" + param;
 
-		String url2 = "https://" + instance.getHost() + ":" + instance.getPort() + "/" + msService.replaceFirst(Pattern.quote("/"), "")
+		String url2 = url + "/" + msService.replaceFirst(Pattern.quote("/"), "")
 				+ "/" + msFunction + "/" + param;
 
 		if (mvmValue != null && !mvmValue.keySet().isEmpty()) {
