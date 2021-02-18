a0fe6c954a3ad85264705c41473b3cb05c0de60b
Hong Onn Lim <hongonn@yahoo.com>
2021-02-18 18:03:29 +0800

    Revert hot fix, use data patch.

--- src/main/java/com/acceval/core/repository/BaseMongoRepositoryImpl.java
+++ src/main/java/com/acceval/core/repository/BaseMongoRepositoryImpl.java
@@ -243,7 +243,6 @@ public abstract class BaseMongoRepositoryImpl<T> implements BaseMongoRepository<
 
 		// base entity
 		if (!isOrCriteria && (!mapParam.containsKey("recordStatus") || StringUtils.isBlank(mapParam.getFirst("recordStatus")))
-				&& (!mapParam.containsKey("status") || StringUtils.isBlank(mapParam.getFirst("status")))
 				&& (BaseEntity.class.isAssignableFrom(targetClass) || BaseCompanyEntity.class.isAssignableFrom(targetClass))) {
 			acceCriteria.appendCriterion(new Criterion("recordStatus", STATUS.ACTIVE, true));
 		}
