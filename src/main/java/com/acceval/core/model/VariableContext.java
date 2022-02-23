d36c01c775e3430115beb6f4e97f8bf047523d71
YKChing <yoong.khang.ching@acceval-intl.com>
2022-02-23 19:44:45 +0800

    STE-1740 [WEB] PCMOS: LOA Approval Changes

--- src/main/java/com/acceval/core/model/VariableContext.java
+++ src/main/java/com/acceval/core/model/VariableContext.java
@@ -72,6 +72,7 @@ public class VariableContext implements Serializable, Cloneable {
 	public static final String OVERWRITE_EXCHANGE_RATE_CTS = "OVERWRITE_EXCHANGE_RATE_CTS";
 	public static final String OVERWRITE_CURRENCY_FROM = "OVERWRITE_CURRENCY_FROM";
 	public static final String OVERWRITE_CURRENCY_TO = "OVERWRITE_CURRENCY_TO";
+	public static final String IS_OVERSEAS = "IS_OVERSEAS";
 
     public static final String MARKET_PRICE_SOURCE = "MARKET_PRICE_SOURCE";
 
