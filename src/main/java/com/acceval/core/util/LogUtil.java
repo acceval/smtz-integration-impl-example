a6f6f99967c8df788d810cb381f4f802e3a2a22f
teybhacc <dylantey97@gmail.com>
2022-08-15 09:19:45 +0800

    fix naming

--- src/main/java/com/acceval/core/util/LogUtil.java
+++ src/main/java/com/acceval/core/util/LogUtil.java
@@ -8,7 +8,7 @@ import ch.qos.logback.core.rolling.FixedWindowRollingPolicy;
 public class LogUtil extends FixedWindowRollingPolicy {
 	@Override
 	public void setFileNamePattern(String fnp) {
-		String fnp1 = fnp.substring(0, 11);
+		String fnp1 = fnp.substring(0, 10);
 		String fnp2 = fnp.substring(11, fnp.length());
 		super.setFileNamePattern(fnp1 + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + fnp2);
 	}
